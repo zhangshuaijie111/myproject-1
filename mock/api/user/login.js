666e57df9fb8670d176910461d4716957171eb40
gxd3 <guoxiaodong167@gmail.com>
2023-07-24 10:57:36 +0800

    feat: 模拟用户登陆

--- mock/api/user/login.js
+++ mock/api/user/login.js
@@ -5,7 +5,20 @@
 module.exports = {
     post(req, res, mock) {
         res.send(mock({
-            msg: '登陆成功'
+            status: 0,
+            msg: '登陆成功',
+            data: {
+                token: '@string(symbol, 3)@string(number, 3)@string(low, 3)@string(upper, 3)'
+            }
+        }));
+    },
+    get(req, res, mock) {
+        res.send(mock({
+            status: 0,
+            msg: '登陆成功',
+            data: {
+                token: '@string(symbol, 6)@string(number, 6)@string(low, 6)@string(upper, 6)'
+            }
         }));
     }
 }
